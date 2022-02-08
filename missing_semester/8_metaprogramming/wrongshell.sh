6f4652f57143eb75db9eb52388799ab91b1bc214
Luchao Qi <lc.qi@outlook.com>
2022-02-08 15:41:37 -0500

    Correct wrongshell syntax

--- missing_semester/8_metaprogramming/wrongshell.sh
+++ missing_semester/8_metaprogramming/wrongshell.sh
@@ -1,7 +1,11 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 echo test some wrong command
-if 1
+# if 1
+# then
+# echo test if else
+# fi
+echo now test some correct command
+if true
 then
-echo test if else
+echo test if else syntax
 fi
-
